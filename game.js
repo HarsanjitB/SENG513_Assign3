5231ea817d955ce3865bb9aeb28fa235f751a30c
Harsanjit Bhullar <harsanjitb1@gmail.com>
2023-10-20 08:29:21 -0600

    Initial commit (web-proj-ass2t)

---
+++ game.js
@@ -0,0 +1,77 @@
+// Initialization Functions
+function initializeGame() {
+    // Sets up the game, called when the game starts.
+}
+
+function resetGame() {
+    // Resets the game to its initial state.
+}
+
+// Player Movement Functions
+function movePlayer1() {
+    // Handles Player 1 movement.
+}
+
+function movePlayer2() {
+    // Handles Player 2 movement.
+}
+
+// Collision Detection
+function checkCollision() {
+    // Check if the snake hits the boundaries or itself.
+}
+
+// Eating and Power-ups
+function eatFood() {
+    // Handles what happens when the snake eats food.
+}
+
+function activatePowerUp() {
+    // Activates the power-up for the respective player.
+}
+
+function clearGridPowerUp() {
+    // Clears all power-ups/bombs from the grid.
+}
+
+// Game State and Scoring
+function updateScore(player, points) {
+    // Updates the score for the given player.
+}
+
+function pauseGame() {
+    // Pauses the game.
+}
+
+function endGame() {
+    // Ends the game and displays the winner.
+}
+
+// UI and Sound Updates
+function toggleMusic() {
+    // Toggles the game music on/off.
+}
+
+function toggleSoundEffects() {
+    // Toggles the game sound effects on/off.
+}
+
+function displayPowerUpIcon(player, powerUpType) {
+    // Displays the current power-up icon next to the player's score.
+}
+
+// Utility Functions
+function generateFood() {
+    // Randomly generates food on the game board.
+}
+
+function generatePowerUp() {
+    // Randomly generates a power-up on the board.
+}
+
+function isValidMove(direction, player) {
+    // Checks if the new direction is a valid move.
+}
+
+// This can be used to initialize the game when the page loads.
+window.onload = initializeGame;
