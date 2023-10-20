6870e6579ea7dfb29fc7bcc42f98c428c5056acd
Harsanjit Bhullar <harsanjitb1@gmail.com>
2023-10-20 14:05:09 -0600

    Commented functions to discuss what they will do.

--- game.js
+++ game.js
@@ -6,34 +6,65 @@
 
 // Initialization Functions
 function initializeGame() {
-    // Sets up the game, called when the game starts.
+    // 1. Set up the game grid.
+    // 2. Place the snakes (player 1 and player 2) at their starting positions.
+    // 3. Generate the initial food item on the grid.
+    // 4. Set initial scores to 0 for both players.
+    // 5. Display any initial instructions or UI elements.
+    // 6. Wait for the player to press the "Start" button.
 }
 
 function resetGame() {
+    // 1. Clear the game grid.
+    // 2. Call the initializeGame function.
     // Resets the game to its initial state.
 }
 
 // Player Movement Functions
 function movePlayer1() {
     // Handles Player 1 movement.
+    // 1. Check the direction the player is moving.
+    // 2. Update the player's position on the grid based on the direction.
+    // 3. Check for collisions using the checkCollision function.
+    // 4. Check if the player eats any food using the eatFood function.
+    // 5. Update the game state.
 }
 
 function movePlayer2() {
     // Handles Player 2 movement.
+    // 1. Check the direction the player is moving.
+    // 2. Update the player's position on the grid based on the direction.
+    // 3. Check for collisions using the checkCollision function.
+    // 4. Check if the player eats any food using the eatFood function.
+    // 5. Update the game state.
 }
 
 // Collision Detection
 function checkCollision() {
     // Check if the snake hits the boundaries or itself.
+    // 1. Check the boundaries of the game grid.
+    //    - If the snake goes outside the boundaries, end the game.
+    // 2. Check if the snake collides with itself.
+    //    - If yes, end the game.
+    // 3. Check if the snake collides with the other player's snake.
+    //    - If yes, end the game.
 }
 
 // Eating and Power-ups
 function eatFood() {
     // Handles what happens when the snake eats food.
+    // 1. Check if the snake's head position matches the food's position.
+    // 2. If yes:
+    //    - Increase the snake's length.
+    //    - Update the score.
+    //    - Generate a new food item on the grid using the generateFood function.
 }
 
 function activatePowerUp() {
     // Activates the power-up for the respective player.
+    // 1. Check the type of power-up.
+    // 2. Activate the effect of the power-up.
+    // 3. Show a timer or indicator for the duration of the power-up.
 }
 
 function clearGridPowerUp() {
@@ -43,6 +74,9 @@ function clearGridPowerUp() {
 // Game State and Scoring
 function updateScore(player, points) {
     // Updates the score for the given player.
+    // 1. Take the player and points as parameters.
+    // 2. Update the player's score by adding the given points.
+    // 3. Display the new score on the UI.
 }
 
 function pauseGame() {
@@ -51,6 +85,8 @@ function pauseGame() {
 
 function endGame() {
     // Ends the game and displays the winner.
+    // 1. Display the winner based on scores or conditions.
+    // 2. Show an option to restart or go back to the main menu.
 }
 
 // UI and Sound Updates
@@ -73,10 +109,16 @@ function generateFood() {
 
 function generatePowerUp() {
     // Randomly generates a power-up on the board.
+    // 1. Randomly select a position on the game grid.
+    // 2. Ensure the selected position is not occupied by any snake.
+    // 3. Place the food item on the selected position.
 }
 
 function isValidMove(direction, player) {
     // Checks if the new direction is a valid move.
+    // 1. 1. Check if the new direction is opposite to the current direction (e.g., can't go from "up" to "down" immediately).
+    // 2. If yes, return false.
+    // 3. Else, return true.
 }
 
 // This can be used to initialize the game when the page loads.
